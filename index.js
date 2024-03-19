0169dd61effce50897d383e0bd98bdcedbdd3872
MutisyaVuta <nicholas.mutisya77@gmail.com>
2024-03-19 19:23:04 +0300

    rewrites assignments and tests

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2
+
+let random;
+random = Math.floor(Math.random()*10) + 1;
+
+let mod;
+const num3 = 10;
+const num4 = 6;
+mod = num3 % num4;
+
+let max = Math.max(14,17,19,5,20)
+
+
+
+
+
